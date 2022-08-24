b7414a6f3063494f1f93e576a988904b947be3f2
petrpu <petri.pu@gmail.com>
2022-08-24 19:13:01 +0300

    tasks updated

---
+++ mod02/04-3numbers.py
@@ -0,0 +1,13 @@
+num1 = int(input("Input first number: "))
+num2 = int(input("Input second number: "))
+num3 = int(input("Input third number: "))
+
+# math time
+sum = num1 + num2 + num3
+product = num1 * num2 * num3
+average = sum/3
+
+# prints
+print(f"Sum of your numbers is {sum}")
+print(f"Product of your numbers is {product}")
+print(f"Average of your numbers is {average}")
